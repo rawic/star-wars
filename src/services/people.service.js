b31ccdbc08d92a0be90f30b2524362d49993e2bc
rawic <hi@rawic.me>
2020-12-15 06:59:39 +0100

    Fix: search of undefined bug

--- src/services/people.service.js
+++ src/services/people.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function fetchPeople({ pageParam = 1, queryKey }) {
   const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}?search=${queryKey[1].search}&page=${pageParam}`
+    `${process.env.NEXT_PUBLIC_API_URL}?search=${queryKey[1]?.search}&page=${pageParam}`
   );
 
   return data;
