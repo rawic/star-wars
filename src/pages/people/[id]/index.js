e711a99ea388ffa0f39d4bf60ebd56486a4602bf
rawic <hi@rawic.me>
2020-12-15 07:11:39 +0100

    Fix: build error

--- src/pages/people/[id]/index.js
+++ src/pages/people/[id]/index.js
@@ -1,14 +1,48 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import Layout from '@layout/Default';
-import { Text, FavoriteButton } from '@components';
+import { Text, FavoriteButton, Button } from '@components';
 import * as S from './styles';
 import { QueryClient, useQuery } from 'react-query';
 import { dehydrate } from 'react-query/hydration';
 import { fetchPerson, auth } from 'services';
 import { personActions, favoritesActions } from '@redux/actions';
+
+export const Wrapper = styled.article`
+  background-color: #272635;
+  border-radius: 1.2rem;
+  padding: 1.5rem 2.5rem;
+  position: relative;
+`;
+
+export const PersonDetails = styled.ul`
+  font-size: ${({ theme }) => theme.fontSize.s};
+  font-weight: ${({ theme }) => theme.fontWeight.semibold};
+`;
+
+export const PersonName = styled.h1`
+  color: ${({ theme }) => theme.highlight};
+  font-size: ${({ theme }) => theme.fontSize.l};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  margin-bottom: 1rem;
+`;
+
+export const FavoriteButtonWrapper = styled.div`
+  position: absolute;
+  right: 2.5rem;
+  top: 1.5rem;
+`;
+
+export const GoBackButton = styled(Button)`
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  height: 3.8rem;
+  margin-top: 3rem;
+  width: 11.5rem;
+`;
 
 const Person = () => {
   const router = useRouter();
