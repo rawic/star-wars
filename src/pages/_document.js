9b5f2d31556370f4205bd00df8e1edeba283c2b1
rawic <hi@rawic.me>
2020-12-15 13:42:16 +0100

    test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            httpEquiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Mukta:wght@400;500;600;700&display=swap"
